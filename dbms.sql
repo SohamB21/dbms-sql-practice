7837e2de03e3f84cce2f5901a464732e623336f4
Soham Banik <soham0110banik@gmail.com>
2024-06-01 12:47:08 +0530

    Update dbms.sql
    fix: commented all the questions

--- dbms.sql
+++ dbms.sql
@@ -76,57 +76,57 @@ INSERT INTO Booking (Hotel_No, Guest_No, Date_From, Date_To, Room_No) VALUES
 ('H437', 'G879', '1999-09-14', '1999-09-17', 223);
 
 
-1.      List full details of all hotels.
+-- 1.      List full details of all hotels.
 SELECT * FROM Hotel;
 
-2.      List full details of all hotels in New York.
+-- 2.      List full details of all hotels in New York.
 SELECT * FROM Hotel WHERE City = 'New York';
 
-3.      List the names and cities of all guests, ordered according to their cities.
+-- 3.      List the names and cities of all guests, ordered according to their cities.
 SELECT Name, City FROM Guest ORDER BY City;
 SELECT Name, City FROM Guest ORDER BY City DESC;
 
-4.      List all details for non-smoking rooms in ascending order of price.
+-- 4.      List all details for non-smoking rooms in ascending order of price.
 SELECT * FROM Room WHERE Type = 'N' ORDER BY Price;
 
-5.      List the number of hotels there are.
+-- 5.      List the number of hotels there are.
 SELECT COUNT(*) AS NumberOfHotels FROM Hotel;
 
-6.      List the cities in which guests live. Each city should be listed only once.
+-- 6.      List the cities in which guests live. Each city should be listed only once.
 SELECT DISTINCT City FROM Guest;
 
-7.      List the average price of a room.
+-- 7.      List the average price of a room.
 SELECT AVG(Price) AS AveragePrice FROM Room;
 
-8.      List hotel names, their room numbers, and the type of that room.
+-- 8.      List hotel names, their room numbers, and the type of that room.
 SELECT Hotel.Name AS Hotel_Name, Room.Room_No, Room.Type AS Room_Type
 FROM Hotel INNER JOIN Room ON Hotel.Hotel_No = Room.Hotel_No;
 
-9.      List the hotel names, booking dates, and room numbers for all hotels in New York.
+-- 9.      List the hotel names, booking dates, and room numbers for all hotels in New York.
 SELECT Hotel.Name AS Hotel_Name, Booking.Date_From, Booking.Date_To, Booking.Room_No 
 FROM Hotel INNER JOIN Booking ON Hotel.Hotel_No = Booking.Hotel_No WHERE City = 'New York';
 
-10.  	What is the number of bookings that started in the month of September?
+-- 10.  	What is the number of bookings that started in the month of September?
 SELECT COUNT(*) AS September_Bookings FROM Booking 
 WHERE STRFTIME('%m', Date_From) = '09';
 
-11.	List the names and cities of guests who began a stay in New York in August.
+-- 11.	List the names and cities of guests who began a stay in New York in August.
 SELECT Guest.Name, Guest.City FROM Guest 
 INNER JOIN Booking ON Guest.Guest_No = Booking.Guest_No
 INNER JOIN Hotel ON Booking.Hotel_No = Hotel.Hotel_No
 WHERE Hotel.City = 'New York' AND STRFTIME('%m', Booking.Date_From) = '08';
 
-12.  	List the hotel names and room numbers of any hotel rooms that have not been booked.
+-- 12.  	List the hotel names and room numbers of any hotel rooms that have not been booked.
 SELECT Hotel.Name, Room.Room_No FROM HOTEL
 INNER JOIN Room ON Hotel.Hotel_No = Room.Hotel_No
 WHERE NOT EXISTS (SELECT * FROM BOOKING WHERE Booking.Room_No = Room.Room_No);
 
-13.  	List the hotel name and city of the hotel with the highest priced room.
+-- 13.  	List the hotel name and city of the hotel with the highest priced room.
 SELECT Hotel.Name, Hotel.City FROM Hotel
 INNER JOIN Room ON Hotel.Hotel_No = Room.Hotel_No
 WHERE Room.Price = (SELECT MAX(Price) FROM Room);
 
-14.  	List hotel names, room numbers, cities, and prices for hotels that have rooms
+-- 14.  	List hotel names, room numbers, cities, and prices for hotels that have rooms
 with prices lower than the lowest priced room in a Boston hotel.
 SELECT Hotel.Name, Room.Room_No, Hotel.City, Room.Price
 FROM Hotel INNER JOIN Room ON Hotel.Hotel_No = Room.Hotel_No
@@ -135,7 +135,7 @@ WHERE Room.Price <
  INNER JOIN Hotel ON Room.Hotel_No = Hotel.Hotel_No
  WHERE City = 'Boston');
 
-15.  	List the average price of a room grouped by city.
+-- 15.  	List the average price of a room grouped by city.
 SELECT AVG(Room.Price) AS AvgPrice, Hotel.City FROM Room 
 INNER JOIN Hotel ON Room.Hotel_No = Hotel.Hotel_No
 GROUP BY Hotel.City;
